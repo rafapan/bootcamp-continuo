14ab7215d38109cba0a1b0de785d3af9911edaac
Rafael Panadero <you@example.comrpanadero@transnational.es>
2022-11-10 20:13:20 +0100

    laboratorio modulo 1

---
+++ Laboratorio - Módulo 1/js/lab1.js
@@ -0,0 +1,5 @@
+document.getElementById("name").value = "Mi nombre";
+document.getElementById("lastName").value = "Primer apellido";
+document.getElementById("avatar").src = "img/avatar.jpg";
+document.getElementById("avatar").height ="96";
+document.getElementById("avatar").width ="96";
