bb03b19e24a1e7fc4c2e094d97cd5be4f0887e65
Rafael Panadero <you@example.comrpanadero@transnational.es>
2022-11-15 17:50:52 +0100

    js terminado

--- Modulo 2 - Variables y tipos de datos/Laboratorio advance/js/app.js
+++ Modulo 2 - Variables y tipos de datos/Laboratorio advance/js/app.js
@@ -1,39 +1,52 @@
+// Definimos el objeto "hotel" y todas su propiedades.
+
 var hotel = {
-    Royal : {
-        img : "https://www.viajessrilanka.es/wp-content/uploads/2018/03/6-FourSeason.jpg",
-        nombre : "Royal Al-Andalus",
-        localizacion : "Torremolinos (Málaga)",
-    },
-    Oasis : {
-        img : "https://cdn1.intriper.com/wp-content/uploads/2022/01/12094557/hoteles-de-lujo-soneva-javi-maldivas-1024x768.jpg",
-        nombre : "Oasis Plaza",
-        localizacion : "Benidorm (Alicante)",
-    },
-    puntuacion : estrellas,
-    firma: "",
-} 
+  Royal: {
+    img: "https://www.viajessrilanka.es/wp-content/uploads/2018/03/6-FourSeason.jpg",
+    nombre: "Royal Al-Andalus",
+    localizacion: "Torremolinos (Málaga)",
+  },
+  Oasis: {
+    img: "https://cdn1.intriper.com/wp-content/uploads/2022/01/12094557/hoteles-de-lujo-soneva-javi-maldivas-1024x768.jpg",
+    nombre: "Oasis Plaza",
+    localizacion: "Benidorm (Alicante)",
+  },
+  puntuacion: {
+    1: '<i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>',
+    2: '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>',
+    3: '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>',
+    4: '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>',
+    5: '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
+  },
+  firma: "",
+};
 
-var estrellas = {
-    1 : '<i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>',
-    2 : '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>',
-    3 : '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i>',
-    4 : '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="far fa-star"></i>',
-    5 : '<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
-}
+// Preguntamos al usuario qué hotel quiere valorar. Para ello creamos una variable cuyo valor pa a definir el usuario.
 
-var hotelseleccionado = prompt('Indica el hotel sobre el que quieres hacer la reseña: \"Royal"\ o \"Oasis\"');
+var hotelseleccionado = prompt(
+  'Indica el hotel sobre el que quieres hacer la reseña: "Royal" o "Oasis"'
+);
 
-var estrellasNumber = prompt("Indica una puntuación del 1 al 5. Siendo 5 la puntuacion más favorable");
+// Para las estrellas nuevamente creamos otra variable para obtener el valor de la puntuacion.
+var estrellaspuntuacion = prompt(
+  "Indica una puntuación del 1 al 5. Siendo 5 la puntuacion más favorable"
+);
 
-// hotel.puntuacion = prompt("Indica una puntuación del 1 al 5. Siendo 5 la puntuacion más favorable");
-hotel.firma = confirm("¿Quiere que la reseña sea anónima?")
+// Para indicar si la reseña será anónima o no le preguntamos al usuario a través la funcion confirm para obtener una respuesta "true" o "false"
+
+hotel.firma = confirm("¿Quiere que la reseña sea anónima?");
+
+//  La var hotelseleccionado nos indica a que propiedad objeto del objeto hotel acceder para cargar sus propiedades.
 
 document.getElementById("img-hotel").src = hotel[hotelseleccionado].img;
-document.getElementById("nombre-hotel").innerHTML = "Hotel " + hotel[hotelseleccionado].nombre;
-document.getElementById("localizacion-hotel").innerHTML = hotel[hotelseleccionado].localizacion;
-// document.getElementById("puntuacion-hotel").innerHTML = hotel.puntuacion;
+document.getElementById("nombre-hotel").innerHTML =
+  "Hotel " + hotel[hotelseleccionado].nombre;
+document.getElementById("localizacion-hotel").innerHTML =
+  hotel[hotelseleccionado].localizacion;
 
-document.getElementById("puntuacion-hotel").innerHTML = estrellas[estrellasNumber];
+// El valor que nos haya proporcionado el usuario nos indicará la propiedad a la que debemos acceder de hotel.puntuacion, pintando así su valor.
+document.getElementById("puntuacion-hotel").innerHTML =
+  hotel.puntuacion[estrellaspuntuacion];
 
-
+// Accedemos al atributo checked del input checkbox. Si el usuario nos devuelve "true" se marcará, si es "false" no.
 document.getElementById("sign-hotel").checked = hotel.firma;
