223857180061f3fd7df04968096f23aa345b6e8f
Rafael Panadero <you@example.comrpanadero@transnational.es>
2023-01-11 21:53:12 +0100

    reserva hotel opcional

--- Modulo 4 - Funciones y eventos/Laboratorio Extra/Reservas-en-hotel-opcional/js/app.js
+++ Modulo 4 - Funciones y eventos/Laboratorio Extra/Reservas-en-hotel-opcional/js/app.js
@@ -5,35 +5,60 @@ var getEmplacement = () => document.getElementById("emplacement-room").value;
 var getNights = () => parseInt(document.getElementById("numberNight").value);
 var getParking = () => parseInt(document.getElementById("numberParking").value);
 
-var priceParking = 0;
+// var priceParking = 0;
+var valueRoom;
+var ocupacion;
 
 // Averiguamos el valor de la habitacion por noche según el tipo de habitación;
-switch(getTypeRoom()) {
-    case "junior": valueRoom = 120; break;
-    case "suite": valueRoom = 150; break;
-    default: valueRoom = 100; break;
+function typeRoom () {
+    // getTypeRoom();
+    switch(getTypeRoom()) {
+        case "junior": valueRoom = 120; break;
+        case "suite": valueRoom = 150; break;
+        default: valueRoom = 100; break;
+    }
+    addSpa();
+    return valueRoom;
 }
 
-// Si hay Spa el valor de la habitación incrementa en 20 €
-var valueRoom = getSpa() == true ? valueRoom + 20 : valueRoom;;
+function addSpa () {
+    // getSpa();
+    valueRoom = getSpa() == true ? valueRoom + 20 : valueRoom;
+    return valueRoom;
+}
+
 
 // Ya sabemos el valor de la habitación y vamos a incrementarle el precio o abaratarlo según la ocupación
-switch(getEmplacement()) {
-    case "triple": valueRoom = valueRoom + (valueRoom * 0.25); break;
-    case "individual": valueRoom = valueRoom - (valueRoom * 0.25); break;
-    default: valueRoom = valueRoom; break;
+
+function emplacementRoom () {
+    // getEmplacement();
+    switch(getEmplacement()) {
+        case "triple": ocupacion = 1.25; break;
+        case "individual": ocupacion = 0.75; break;
+        default: ocupacion = 1; break;
+    }
+    return ocupacion;
 }
+
 
 // vamos a comprobar si hay reservas para el parking y cada dia vale 10 €
 var calcParking = () => getParking() * 10;
 
-var priceTotal = () => (valueRoom * getNights()) + calcParking();
+// var priceTotal = () => (((typeRoom() * emplacementRoom()) * getNights()) + calcParking());
 
+var priceTotal = () => ((typeRoom() * emplacementRoom()) * getNights()) + calcParking();
 
 function resultTotal() {
-    priceTotal(); 
-    document.getElementById("printResult").innerText = priceTotal();
+    if (isNaN(priceTotal())) {
+        document.getElementById("printResult").innerText = "Completa correctamente los campos";
+    } else {
+        document.getElementById("printResult").innerText = priceTotal();
+    }
 }
 
-
-document.getElementById("getResult").addEventListener("click", resultTotal);
+resultTotal();
+document.getElementById("type-room").addEventListener("change", resultTotal);
+document.getElementById("add-spa").addEventListener("change", resultTotal);
+document.getElementById("emplacement-room").addEventListener("change", resultTotal);
+document.getElementById("numberNight").addEventListener("change", resultTotal);
+document.getElementById("numberParking").addEventListener("change", resultTotal);
