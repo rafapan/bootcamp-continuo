a8a81d17703957f0b54550289edf2931930f1d36
Rafael Panadero <you@example.comrpanadero@transnational.es>
2023-01-12 23:59:27 +0100

    calculadora secuencial fin

--- Modulo 4 - Funciones y eventos/Laboratorio Extra/Calculadora Secuencial/js/calculator-sec2.js
+++ Modulo 4 - Funciones y eventos/Laboratorio Extra/Calculadora Secuencial/js/calculator-sec2.js
@@ -1,15 +1,14 @@
 var operando = 0;
 var getNumber = () => parseInt(document.getElementById("input-number").value);
 var operacion = "";
-var resultadoFinal = () =>
-  parseInt((document.getElementById("result").innerText = resultado));
-var resultado;
-console.log(resultado);
+var resultadoFinal = () => parseInt((document.getElementById("result").innerText = resultado));
 
-function resetCount() {
-  resultado = undefined;
-  operacion = "";
-}
+var resultado = 0;
+console.log(resultado);
+console.log(typeof(resultado));
+console.log(getNumber());
+console.log(typeof(getNumber()));
+
 
 function getResultadoParcial(operacion) {
   switch (operacion) {
@@ -28,59 +27,86 @@ function getResultadoParcial(operacion) {
     default:
       resultado = operando;
   }
+  resetOperacion();
 }
+function resetOperacion() {
+  document.getElementById("input-number").value = "";
+  document.getElementById("input-number").focus();
+  operacion = "";
+}
+
+// function sumar() {
+//   console.log(resultado);
+//   console.log(typeof(resultado));
+
+//   operando = parseInt(document.getElementById("input-number").value);
+//   getResultadoParcial(operacion);
+//   operacion = "+";
+// }
 
 function sumar() {
-  operando = parseInt(document.getElementById("input-number").value);
-
-  getResultadoParcial(operacion);
-
-  operacion = "+";
-
-  console.log("El resultado parcial es:", resultado);
-  console.log("El número introducido es:", operando);
+  console.log(getNumber());
+  console.log(typeof(getNumber()));
+  if (isNaN(getNumber())) {
+    operando = parseInt((document.getElementById("result").innerText));
+    console.log(operando);
+    getResultadoParcial(operacion);
+    operacion = "+";
+  } else {
+    operando = parseInt(document.getElementById("input-number").value);
+    console.log(operando);
+    getResultadoParcial(operacion);
+    operacion = "+";
+  }
 }
+
 
 function restar() {
   operando = parseInt(document.getElementById("input-number").value);
-
   getResultadoParcial(operacion);
-
   operacion = "-";
-
-  console.log("El resultado parcial es:", resultado);
-  console.log("El número introducido es:", operando);
 }
 
 function multiply() {
   operando = parseInt(document.getElementById("input-number").value);
-
   getResultadoParcial(operacion);
-
   operacion = "*";
-
-  console.log("El resultado parcial es:", resultado);
-  console.log("El número introducido es:", operando);
 }
 
 function divide() {
   operando = parseInt(document.getElementById("input-number").value);
-
   getResultadoParcial(operacion);
-
   operacion = "/";
-
-  console.log("El resultado parcial es:", resultado);
-  console.log("El número introducido es:", operando);
 }
 
 function result() {
   operando = getNumber();
   getResultadoParcial(operacion);
-  resultadoFinal();
+
+  if (isNaN(resultado)) {
+    document.getElementById("result").innerText = "Error";
+  } else {
+    resultadoFinal();
+  }
+
+  resetResultado();
+
   console.log(resultado);
-  resetCount();
+  console.log(typeof(resultado));
+}
+
+function reset () {
+  document.getElementById("input-number").value = "";
+  document.getElementById("result").innerText = "0";
+  resetOperacion();
+  resetResultado();
   console.log(resultado);
+  console.log(operacion);
+  console.log(typeof(resultado));
+}
+
+function resetResultado () {
+  resultado = parseInt(document.getElementById("result").innerText);
 }
 
 document.getElementById("btn-add").addEventListener("click", sumar);
@@ -89,3 +115,4 @@ document.getElementById("btn-multiply").addEventListener("click", multiply);
 document.getElementById("btn-divide").addEventListener("click", divide);
 
 document.getElementById("btn-resolve").addEventListener("click", result);
+document.getElementById("btn-reset").addEventListener("click", reset);
