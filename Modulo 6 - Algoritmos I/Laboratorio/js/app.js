4d2e42ce942fb15ab1a95ee6b595acfd525b36ac
Rafael Panadero <you@example.comrpanadero@transnational.es>
2023-02-07 10:15:33 +0100

    boton disabled

--- Modulo 6 - Algoritmos I/Laboratorio/js/app.js
+++ Modulo 6 - Algoritmos I/Laboratorio/js/app.js
@@ -68,80 +68,112 @@ const products = [
 // - Pintar nombre del producto seleccionado
 // - Pintar precio por unidad
 
-
-
 var showProducts = (productList) => {
-    var cart = document.getElementById("product-list-container");
+  var cart = document.getElementById("product-list-container");
 
-    for (const product of productList) {
-      var div = document.createElement("li");
-      div.setAttribute("class", "cart-item");
-  
-      var descriptionProduct = document.createElement("div");
-      descriptionProduct.setAttribute("class", "item-product")
-      descriptionProduct.innerText = product.description;
+  for (const product of productList) {
+    var div = document.createElement("li");
+    div.setAttribute("class", "cart-item");
 
-      var priceProduct = document.createElement("div");
-      priceProduct.setAttribute("class", "item-price")
-      priceProduct.innerText = product.price + " €/ud.";
+    var descriptionProduct = document.createElement("div");
+    descriptionProduct.setAttribute("class", "item-product");
+    descriptionProduct.innerText = product.description;
+
+    var priceProduct = document.createElement("div");
+    priceProduct.setAttribute("class", "item-price");
+    priceProduct.innerText = product.price + " €/ud.";
+
+    var input = document.createElement("input");
+    input.setAttribute("class", "product-unit");
+    input.setAttribute("type", "number");
+    input.setAttribute("placeholder", product.units);
+    input.setAttribute("min", 0);
+    input.setAttribute("max", product.stock);
+    input.addEventListener(
+      "change",
+      (event) => (product.units = event.target.value)
+    );
+
+    cart.appendChild(div).appendChild(descriptionProduct);
+    cart.appendChild(div).appendChild(priceProduct);
+    cart.appendChild(div).appendChild(input);
+  }
   
-      var input = document.createElement("input");
-      input.setAttribute("class", "product-unit");
-      input.setAttribute("type", "number");
-      input.setAttribute("value", product.units);
-      input.setAttribute("min", 0);
-      input.setAttribute("max", product.stock);
-      input.addEventListener(
-        "change",
-        (event) => (product.units = event.target.value)
-      );
-  
-      
-      cart.appendChild(div).appendChild(descriptionProduct);
-      cart.appendChild(div).appendChild(priceProduct);
-      cart.appendChild(div).appendChild(input);
-    }
-  };
+};
 showProducts(products);
-
 
 // PARTE 2: Calcular factura y mostrar desglose final del precio
 //  Pintar subtotal
 
 //  Calcular subtotal. Es el precio sin IVA
-var subtotal = productList => {
-    var subtotalCart = 0;
-    for (var object of productList) {
-        subtotalCart += object.units * object.price;
-    }
-    return subtotalCart;
+var subtotal = (productList) => {
+  var subtotalCart = 0;
+  for (var object of productList) {
+    subtotalCart += object.units * object.price;
+  }
+  return subtotalCart;
 };
 
 // Calcular IVA. Es el total del IVa de todos los productos
-var IVA = productList => {
-    var iva = 0;
-    for (var object of productList) {
-        iva += object.units * object.price * object.tax / 100;
-    }
-    return iva;
-}
+var IVA = (productList) => {
+  var iva = 0;
+  for (var object of productList) {
+    iva += (object.units * object.price * object.tax) / 100;
+  }
+  return iva;
+};
 
 // Calcular el total. Es el subtotal + el IVA
-var total = productList => {
-    var total = 0;
-    total = subtotal(productList) + IVA(productList);
-    return total;
-}
-
+var total = (productList) => {
+  var total = 0;
+  total = subtotal(productList) + IVA(productList);
+  return total;
+};
 
 var calculate = () => {
-    
-    console.log(subtotal(products));
-    document.getElementById('subtotal').innerText = subtotal(products) + " €";
-    document.getElementById('taxes').innerText = IVA(products) + '€';
-    document.getElementById('total').innerText = total(products).toFixed(2) + '€';
+  document.getElementById("subtotal").innerText = subtotal(products) + " €";
+  document.getElementById("taxes").innerText = IVA(products) + "€";
+  document.getElementById("total").innerText = total(products).toFixed(2) + "€";
 };
-  
-document.getElementById('calculate').addEventListener('click', calculate);
+
+document.getElementById("calculate").addEventListener("click", calculate);
 
 // Extra: Intenta hacer que el botón Calcular se habilite o deshabilite en función de si el usuario ha elegido al menos 1 unidad de algún producto o no
+
+// var isproductAdded = (productList) => {
+//   var numberobjects = 0;
+//   for (object of productList) {
+//       numberobjects += object.units;
+//   };
+//   if (numberobjects > 0) {
+//     document.getElementById("calculate").disabled = false;
+//   } else {
+//     document.getElementById("calculate").disabled = true;
+//   };
+// }
+
+// document.getElementById("comprobar").addEventListener("click", isproductAdded(products));
+
+var isproductAdded = () => {
+  var numberobjects = 0;
+  var input = document.getElementById("calculate");
+  for (object of products) {
+    numberobjects += object.units;
+  }
+  if (numberobjects > 0) {
+    input.disabled = false;
+  } else {
+    input.disabled = true;
+  }
+  return input.disabled;
+};
+
+
+
+let allProductUnits = document.getElementsByClassName('product-unit');
+for (let i = 0; i < allProductUnits.length; i++) {
+  allProductUnits[i].addEventListener("focus", isproductAdded);
+  allProductUnits[i].addEventListener("keyDown", isproductAdded);
+  allProductUnits[i].addEventListener("keyUp", isproductAdded);
+  allProductUnits[i].addEventListener("keyPress", isproductAdded);
+}
