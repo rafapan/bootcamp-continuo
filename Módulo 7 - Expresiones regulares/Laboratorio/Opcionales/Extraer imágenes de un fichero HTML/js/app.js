3b73a0e8951234381be96454e66e276776aa5679
Rafael Panadero <you@example.comrpanadero@transnational.es>
2023-05-07 20:45:17 +0200

    modulo 7

--- Módulo 7 - Expresiones regulares/Laboratorio/Opcionales/Extraer imágenes de un fichero HTML/js/app.js
+++ Módulo 7 - Expresiones regulares/Laboratorio/Opcionales/Extraer imágenes de un fichero HTML/js/app.js
@@ -1,17 +1,20 @@
-// const patterncase1 = /<img src="(.*)"/;
+const patterncase1 = /<img src="(.*)"/;
 
-// const valuescase1 = [
-//   '<img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/>',
-// ];
+const valuescase1 = [
+  '<img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/>',
+];
 
-// valuescase1.forEach((value) => {
-//   console.log("Caso 1 " + value + " -> ", value.match(patterncase1));
-// });
-
-const patterncase2 = /<img\s?src="(.*)"\/>/;
-const valuescase2 =
-  ['<html>', '<body>, <img src="https://image.freepik.com/iconos-gratis/github-circular_318-10610.jpg"/>', '<h1>ejemplo</h1>, <img src="https://github.githubassets.com/images/modules/logos_page/GitHubMark.png"/>, </body>, </html>'];
-
-  valuescase2.forEach((value) => {
-  console.log("Caso 2 " + value.match(patterncase2));
+valuescase1.forEach((value) => {
+  console.log("Caso 1 " + value + " -> ", value.match(patterncase1));
 });
+
+const patterncase2 = /<img\s?src="(.*)"\/>/gim;
+const valuescase2 = `<html>
+                    <body>
+                        <img src="https://image.freepik.com/iconos-gratis/github-circular_318-10610.jpg"/>
+                        <h1>ejemplo</h1>
+                        <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/>
+                    </body>
+                    </html>`;
+
+console.log(valuescase2.match(patterncase2));
